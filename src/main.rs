8479df12c34991a51a874444454fcb0250fdb340
Yeray Romero <yerayromerodev@gmail.com>
2024-02-04 18:36:39 +0100

    Added deterministic sorting

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,46 @@
 mod model;
 use model::merge_abstraction::MergeAbstraction;
 #[cfg(test)]
-    mod tests {
-        use super::*;
-        #[test]
-        fn abstraction_line_count() {
-            let test_string: String = "Hello\nWorld\n".to_string();
-            let test_abstraction: MergeAbstraction = MergeAbstraction::construct_base(test_string);
-            assert_eq!(test_abstraction.line_count, 2);
-        }
-        #[test]
-        fn merge_trivial_basic() {
-            // Base represents all aces of a deck of poker cards.
-            // Imagine in alfa, we add the nine of spades.
-            // Imagine in bravo, we simply remove the Ace of Diamonds.
-            let base: String = "AceHearts\nAceSpades\nAceClubs\nAceDiamonds\n".to_string();
-            let alfa: String = "AceHearts\nAceSpades\nAceClubs\nAceDiamonds\nNineSpades\n".to_string();
-            let bravo: String = "AceHearts\nAceSpades\nAceClubs\n".to_string();
-            let result: String = merge(base, alfa, bravo);
-            assert_eq!(result, "AceHearts\nAceSpades\nAceClubs\nNineSpades\n");
-        }
+mod tests {
+    use super::*;
+    #[test]
+    fn abstraction_line_count() {
+        let test_string: String = "Hello\nWorld\n".to_string();
+        let test_abstraction: MergeAbstraction = MergeAbstraction::construct_base(test_string);
+        assert_eq!(test_abstraction.line_count, 2);
     }
+    #[test]
+    fn test_deterministic_sorting() {
+        let string_a = "Hello\nWorld\n".to_string();
+        let string_b = "Hello\nas\n".to_string();
+        let (a, b) = MergeAbstraction::construct_sorted(&string_a, &string_b);
+        let first_result = vec![a.sorting, b.sorting];
+        let (b2, a2) = MergeAbstraction::construct_sorted(&string_b, &string_a);
+        let second_result = vec![a2.sorting, b2.sorting];
+        assert_eq!(first_result, second_result);
+    }
+    #[test]
+    fn merge_trivial_basic() {
+        // Base represents all aces of a deck of poker cards.
+        // Imagine in alfa, we add the nine of spades.
+        // Imagine in bravo, we simply remove the Ace of Diamonds.
+        let base: String = "AceHearts\nAceSpades\nAceClubs\nAceDiamonds\n".to_string();
+        let alfa: String = "AceHearts\nAceSpades\nAceClubs\nAceDiamonds\nNineSpades\n".to_string();
+        let bravo: String = "AceHearts\nAceSpades\nAceClubs\n".to_string();
+        let result: String = merge(base, alfa, bravo);
+        assert_eq!(result, "AceHearts\nAceSpades\nAceClubs\nNineSpades\n");
+    }
+}
 fn main() {
     println!("Deck-Merge, from Riel-Foundation");
 }
 /**
  * Merge returns a string in which all lines of base are present,
  * except the ones that got removed in alfa and bravo,
- * and also all lines that got addded in alfa and bravo. 
+ * and also all lines that got addded in alfa and bravo.
  */
 fn merge(base: String, alfa: String, bravo: String) -> String {
-  todo!()
+    let zero = MergeAbstraction::construct_base(base);
+    let (a, b) = MergeAbstraction::construct_sorted(&alfa, &bravo);
+    todo!()
 }
--- src/model/merge_abstraction.rs
+++ src/model/merge_abstraction.rs
@@ -1,23 +1,44 @@
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+};
+
 pub struct MergeAbstraction {
-    pub sorting: i64, 
+    pub sorting: i64,
     pub lines: Vec<String>,
     pub line_count: u32,
 }
 impl MergeAbstraction {
-  /**
-   * Does not allow for a negative sorting value. For that, see construct_base
-   */
-  fn construct(string: String, sorting: i64) -> MergeAbstraction {
-    return MergeAbstraction {
-      sorting: sorting,
-      lines: string.lines().map(|line: &str| line.to_string()).collect(),
-      line_count: string.lines().count() as u32,
+    /**
+     * Does not allow for a negative sorting value. For that, see construct_base
+     */
+    fn construct(string: String, sorting: i64) -> MergeAbstraction {
+        return MergeAbstraction {
+            sorting: sorting,
+            lines: string.lines().map(|line: &str| line.to_string()).collect(),
+            line_count: string.lines().count() as u32,
+        };
     }
-  }
-  pub fn construct_from_string(string: String, sorting: u32) -> MergeAbstraction {
-    return Self::construct(string, sorting as i64);
-  }
-  pub fn construct_base(string: String) -> MergeAbstraction {
-    return Self::construct(string, -1);
-  }
+    pub fn construct_from_string(string: String, sorting: u32) -> MergeAbstraction {
+        return Self::construct(string, sorting as i64);
+    }
+    pub fn construct_base(string: String) -> MergeAbstraction {
+        return Self::construct(string, -1);
+    }
+    pub fn construct_sorted(a: &String, b: &String) -> (MergeAbstraction, MergeAbstraction) {
+        let hash1: u64 = hash_string(&a);
+        let hash2: u64 = hash_string(&b);
+
+        fn hash_string(string: &str) -> u64 {
+            let mut hasher = DefaultHasher::new();
+            string.hash(&mut hasher);
+            hasher.finish()
+        }
+        let sorting_1: i64 = if hash1 < hash2 { 0 } else { 1 };
+        let sorting_2: i64 = if hash1 < hash2 { 1 } else { 0 };
+        return (
+            Self::construct(a.clone(), sorting_1),
+            Self::construct(b.clone(), sorting_2),
+        );
+    }
 }
